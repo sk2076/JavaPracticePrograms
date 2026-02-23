3a46029c182f11df2c3d00ea46cee3c7eaf738c6
Siva Sagar <sk2076@srmist.edu.in>
2026-02-23 19:16:22 +0530

    Level-2 code3

---
+++ week1/SquareSide.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class SquareSide {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter perimeter of square: ");
+        double perimeter = input.nextDouble();
+        input.close();
+        double side = perimeter / 4;
+
+        System.out.println("The length of the side is " + side +
+                " whose perimeter is " + perimeter);
+    }
+}
