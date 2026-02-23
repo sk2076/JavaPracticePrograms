1c2fc10920e62683b1b6886df2febe37be6236f4
Siva Sagar <sk2076@srmist.edu.in>
2026-02-23 19:22:11 +0530

    Level-2 code5

---
+++ week1/PurchaseTotal.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class PurchaseTotal {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter unit price: ");
+        double unitPrice = input.nextDouble();
+
+        System.out.print("Enter quantity: ");
+        int quantity = input.nextInt();
+        input.close();
+        double totalPrice = unitPrice * quantity;
+
+        System.out.println("The total purchase price is INR "
+                + totalPrice + " if the quantity " + quantity +
+                " and unit price is INR " + unitPrice);
+    }
+}
